1ce1597438a4b303413b5d5dace67bcc2813db21
El viejo firu <juane.galeanob@uqvirtual.edu.co>
2023-08-24 18:23:54 -0500

    Create TestNumero.java

---
+++ Problemas/TestNumero.java
@@ -0,0 +1,15 @@
+public class TestNumero {
+    /*
+     * Determinar si un número es positivo, negativo o cero
+     */
+    public static void main(String[] args) {
+        int numero =5;
+        if (numero>0) {
+            System.out.println("El número es positivo");
+        } else if(numero<0) {
+            System.out.println("El número es negativo");
+        }else{
+            System.out.println("El número es cero");
+        }
+    }
+}
