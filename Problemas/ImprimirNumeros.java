aa11c75a0292f6973b0874c4b011015713752ccd
El viejo firu <juane.galeanob@uqvirtual.edu.co>
2023-08-24 18:48:02 -0500

    Create ImprimirNumeros.java

---
+++ Problemas/ImprimirNumeros.java
@@ -0,0 +1,12 @@
+/*
+ * Imprimir los primeros 5 números enteros
+ */
+public class ImprimirNumeros{
+    public static void main(String[] args) {
+        int numero=1;
+        while (numero<=5) {
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
